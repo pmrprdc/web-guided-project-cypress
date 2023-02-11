9a257d8bbd0ec5985d347b51ad3748104fcb3153
Pamir Perdeci <pamirperdeci@gmail.com>
2023-02-11 18:07:58 -0500

    practice test is working

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,1 +1,43 @@
 // write tests here
+describe("Quotes App", ()=>{
+
+    beforeEach(()=>{
+        cy.visit("http://localhost:1234");
+
+    })
+
+//Helpers
+
+
+const textInput = () => cy.get("input[name=text]");
+const authorInput = () => cy.get("input[name=author]");
+const foobarInput = () => cy.get("input[name=foobar]");
+const submitBtn = () => cy.get(`button[id=submitBtn"]`)
+
+
+
+it("Sanity check to make sure tests work", ()=> {
+
+
+
+    expect(1+2).to.equal(3);
+} )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
