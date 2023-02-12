cb5d3323ada1d1acc365eee9f2d5d5f84f0bf69f
Pamir Perdeci <pamirperdeci@gmail.com>
2023-02-11 20:43:57 -0500

    the propper elements are showing

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -3,7 +3,6 @@ describe("Quotes App", ()=>{
 
     beforeEach(()=>{
         cy.visit("http://localhost:1234");
-
     })
 
 //Helpers
@@ -21,13 +20,24 @@ it("Sanity check to make sure tests work", ()=> {
 
 
     expect(1+2).to.equal(3);
+    expect(2+2).to.equal(4);
+    expect({}).not.to.equal({});
+    
 } )
 
 
 
 
 
+it("THE PROPPER ELEMENTS ARE SHOWING!", ()=>{
 
+
+    textInput().should("exist")
+    authorInput().should("exist");
+    foobarInput().should("not.exist")
+    cy.contains("Submit Quote").should("exist")
+    cy.contains(/submit quote/i).should("exist")
+})
 
 
 
